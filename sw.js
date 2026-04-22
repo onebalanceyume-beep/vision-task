7905448a4943bd4ac517e936a59ec6a91770fa1b
onebalanceyume-beep <one.balance.yume@gmail.com>
2026-04-22 13:15:56 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
 const CACHE_VERSION = 'v' + Date.now();
 
 self.addEventListener('install', (e) => { self.skipWaiting(); });
-
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => {
